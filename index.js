7e2632a33ddfa5367696a88a6cecdae85777dd80
Test <a@a.Com>
2024-04-23 11:02:15 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// Import des différentes dépendances
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+require('dotenv').config();
+
+// Initialisation de l'appli express, on définis le numéor de prot
+let app = express();
+let port = 3000;
+// Message de bienvenue sur localhost:3000
+app.get('/', (req, res) => {
+    res.send('Welcome to my backend');
+});
+// Les routes a utiliser sur localhost:3000/api
+app.use('/api', routes);
+
+// Ici on va se connecter a la base de donnée grâce a Mongoose
+// MONGO_URI='mongodb://localhost:27017/pokemondb/'
+mongoose
+    .connect(process.env.MONGO_URI, {})
+    .then(() => {
+        console.log("Connectés a la BDD");
+    })
+    .catch((err) => {
+        console.log('Erreur de connexion', err)
+    })
+
+// Ici on lance l'application sur le numéro de port en question.
+app.listen(port, () => {
+    console.log("Server en ligne sur le port 3000");
+});
---
+++ routes.js
@@ -0,0 +1,19 @@
+// Import des dépendances
+const express = require('express');
+// Le router est le composant qui va définir ce qu'on fait pour
+// chaque chemin qu'on emprunte
+// Un GET sur http://localhost:3000/users/
+// Va nous ramener sur usersController.getAllusers()
+const router = express.Router();
+const pokemonRoutes = require('./routes/pokemonRoutes');
+
+// Message de bienvenue sur mon API sur localhost:3000/api
+router.get('/', (req, res) => {
+    res.send('Welcome to the API');
+})
+
+// Si l'utilisateur va sur localhost:3000/api/pokemons, on utilise
+// pokemonRoutes pour gérer les routes
+router.use('/pokemons', pokemonRoutes);
+
+module.exports = router;

29fb4530268d5a10cadd9a265140947c4052ce35
Test <a@a.Com>
2024-04-25 09:59:54 +0200

    Login function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require("../models/userModel");
 
 exports.getAllUsers = async (req, res) => {
@@ -17,21 +18,6 @@ exports.getOneUser = async (req, res) => {
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
-  }
-};
-
-exports.register = async (req, res) => {
-  try {
-    const hashedPwd = await bcrypt.hash(req.body.password, 10)
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPwd,
-    });
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
   }
 };
 
@@ -57,6 +43,34 @@ exports.deleteUser = async (req, res) => {
     const deletedUser = await User.findByIdAndDelete(req.params.id);
     if (!deletedUser) return res.status(404).json("User not found.");
     res.status(200).json('User deleted successfully');
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.register = async (req, res) => {
+  try {
+    const hashedPwd = await bcrypt.hash(req.body.password, 10)
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPwd,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email})
+    if (!user) return res.status(404).json('Invalid identifiers');
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(404).json('Invalid identifiers');
+    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+    res.header('auth-token', token).send(token);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,9 @@ router.get('/:id', userController.getOneUser)
 // Create a user
 // POST sur localhost:3000/api/users
 router.post('/', userController.register)
+// Login a user
+// POST sur localhost:3000/api/users/login
+router.post('/login', userController.login)
 // Edit a user
 router.put('/:id', userController.editUser)
 // Delete a user
